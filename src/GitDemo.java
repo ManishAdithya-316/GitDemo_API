95e1282bf6776dff27ff47c20276e673ad0e6b0c
Manish Adithya <adithya.hulugundi@gmail.com>
2026-04-25 21:47:44 +0530

    abc

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -1,4 +1,13 @@
 
+//Tips import project 2 times, can change name in .project folder to make it importable
+//Step 1)Make changes to project
+//Step 2)Use git add * to add to staging
+//Step 3)Use git status to check
+//Step 4)Use git commit -m "commit msg" for final commit
+//Step 5)Use git push origin master to push code to Github
+//Step 6)Use git pull origin master to get latest code ...always pull latest code before starting work
+
+
 public class GitDemo {
 
 	public static void main(String[] args) {
