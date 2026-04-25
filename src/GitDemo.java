496b81b7531a0e5401c5c1cfcea0cff27bf942a9
Manish Adithya <adithya.hulugundi@gmail.com>
2026-04-25 18:19:12 +0530

    Commited by different person Y

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,7 +4,9 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is Git demo of git commands by a Person named X");		
+		System.out.println("This is Git demo of git commands by a Person named X");	
+		
+		System.out.println("This is a line added by a different person named Y");
 	}
 
 }
