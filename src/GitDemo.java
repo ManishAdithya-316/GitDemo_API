fcedb4187426c280ce5b6670a93941e0904263ae
Manish Adithya <adithya.hulugundi@gmail.com>
2026-04-25 18:05:08 +0530

    first commit

---
+++ src/GitDemo.java
@@ -0,0 +1,10 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is Git demo of git commands by a Person named X");		
+	}
+
+}
